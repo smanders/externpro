51483d85fa03b0c7aa7b224ff2ac8bc1574c5c3d
Scott M Anderson <smanders@sdl.usu.edu>
2016-02-23 16:13:33 -0700

    include targets.cmake from use scripts, only if they exist

--- projects/use/usexp-bzip2-config.cmake
+++ projects/use/usexp-bzip2-config.cmake
@@ -6,7 +6,9 @@ set(prj bzip2)
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
 # targets file (-targets) installed to lib/cmake
-include(${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
+if(EXISTS ${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
+  include(${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
+endif()
 string(TOUPPER ${prj} PRJ)
 unset(${PRJ}_INCLUDE_DIR CACHE)
 find_path(${PRJ}_INCLUDE_DIR bzip2/bzlib.h PATHS ${XP_ROOTDIR}/include NO_DEFAULT_PATH)
--- projects/use/usexp-zlib-config.cmake
+++ projects/use/usexp-zlib-config.cmake
@@ -7,7 +7,9 @@ set(prj zlib)
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
 # targets file (-targets) installed to lib/cmake
-include(${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
+if(EXISTS ${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
+  include(${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
+endif()
 string(TOUPPER ${prj} PRJ)
 unset(${PRJ}_INCLUDE_DIR CACHE)
 find_path(${PRJ}_INCLUDE_DIR zlib.h PATHS ${XP_ROOTDIR}/include/${prj} NO_DEFAULT_PATH)
