606c80ef7e05a4d21500d92adbe844e930f7b95d
Scott M Anderson <smanders@sdl.usu.edu>
2016-05-26 09:29:35 -0600

    bump version of wxcmake projects

--- projects/wxcmake31.cmake
+++ projects/wxcmake31.cmake
@@ -7,7 +7,7 @@ set(PRO_WXCMAKE31
   LICENSE "open" http://www.wxwidgets.org/about/newlicen.htm "wxWindows License -- essentially LGPL with an exception stating that derived works in binary form may be distributed on the user's own terms"
   DESC "build wxWidgets via cmake"
   REPO "repo" ${REPO} "wxcmake repo on github"
-  VER 2015.10.02 # latest wx30 branch commit date
+  VER 2016.05.23 # latest wx31 branch commit date
   GIT_ORIGIN git://github.com/smanders/wxcmake.git
   GIT_TAG wx31 # what to 'git checkout'
   GIT_REF wx0 # create patch from this tag to 'git checkout'
