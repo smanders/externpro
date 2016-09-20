90862d5252f3f7ebea48cb77c0f060450a76cc3a
Scott M Anderson <smanders@sdl.usu.edu>
2016-09-19 18:23:45 -0600

    clang-format v3.9.0

--- projects/clang.cmake
+++ projects/clang.cmake
@@ -1,7 +1,8 @@
 # clang
 include(${CMAKE_CURRENT_LIST_DIR}/llvm.cmake)
 xpGetArgValue(${PRO_LLVM} ARG VER VALUE llvmVer)
-set(REPO https://github.com/smanders/clang)
+xpGetArgValue(${PRO_LLVM} ARG GIT_TAG VALUE llvmTag)
+set(REPO https://github.com/llvm-mirror/clang)
 set(PRO_CLANG
   NAME clang
   SUPERPRO llvm
@@ -9,14 +10,10 @@ set(PRO_CLANG
   WEB "clang" http://clang.llvm.org/ "clang website"
   LICENSE "open" "http://clang.llvm.org/features.html#license" "LLVM 'BSD' License"
   DESC "clang: a C language family frontend for LLVM"
-  REPO "repo" ${REPO} "forked clang repo on github"
+  REPO "repo" ${REPO} "clang repo on github"
   VER ${llvmVer}
-  GIT_ORIGIN git://github.com/smanders/clang.git
-  GIT_UPSTREAM git://github.com/llvm-mirror/clang.git
-  GIT_TAG xp${llvmVer} # what to 'git checkout'
-  GIT_REF v${llvmVer} # create patch from this tag to 'git checkout'
+  GIT_ORIGIN git://github.com/llvm-mirror/clang.git
+  GIT_TAG ${llvmTag} # what to 'git checkout'
   DLURL http://llvm.org/releases/${llvmVer}/cfe-${llvmVer}.src.tar.xz
-  DLMD5 4ff2f8844a786edb0220f490f7896080
-  PATCH ${PATCH_DIR}/clang.patch
-  DIFF ${REPO}/compare/
+  DLMD5 29e1d86bee422ab5345f5e9fb808d2dc
   )
--- projects/llvm.cmake
+++ projects/llvm.cmake
@@ -1,6 +1,6 @@
 # llvm
 xpProOption(llvm)
-set(VER 3.8.1)
+set(VER 3.9.0)
 string(REGEX REPLACE "([0-9]+)\\.([0-9]+)(\\.[0-9]+)?" "release_\\1\\2" LLVM_BRANCH ${VER})
 set(REPO https://github.com/llvm-mirror/llvm)
 set(PRO_LLVM
@@ -13,6 +13,6 @@ set(PRO_LLVM
   GIT_ORIGIN git://github.com/llvm-mirror/llvm.git
   GIT_TAG ${LLVM_BRANCH}
   DLURL http://llvm.org/releases/${VER}/llvm-${VER}.src.tar.xz
-  DLMD5 538467e6028bbc9259b1e6e015d25845
+  DLMD5 f2093e98060532449eb7d2fcfd0bc6c6
   SUBPRO clang
   )
