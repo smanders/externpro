f4de08efcf8a73064c4b07d16e7cfb546eeea4f4
Scott M Anderson <smanders@sdl.usu.edu>
2017-08-23 16:07:18 -0600

    libgeotiff version libraries and includes
    * embed version in library name, targets file, and include directories
    * doesn't need wx source directory to build (so that was removed from build_wxv() call and XP_CONFIGURE)
    * see issue https://github.com/smanders/externpro/issues/142

--- projects/libgeotiff.cmake
+++ projects/libgeotiff.cmake
@@ -2,7 +2,7 @@
 xpProOption(libgeotiff)
 # http://packages.debian.org/sid/libgeotiff-dev
 # http://libgeotiff-dfsg.sourcearchive.com/
-set(VER 1.2.4)
+set(GTIFVER 1.2.4)
 set(REPO https://github.com/smanders/libgeotiff)
 set(PRO_LIBGEOTIFF
   NAME libgeotiff
@@ -10,17 +10,17 @@ set(PRO_LIBGEOTIFF
   LICENSE "open" http://trac.osgeo.org/geotiff/ "trac site states it is an open source library (no specific license mentioned)"
   DESC "georeferencing info embedded within TIFF file"
   REPO "repo" ${REPO} "libgeotiff repo on github"
-  VER ${VER}
+  VER ${GTIFVER}
   GIT_ORIGIN git://github.com/smanders/libgeotiff.git
-  GIT_TAG xp${VER} # what to 'git checkout'
-  GIT_REF v${VER} # create patch from this tag to 'git checkout'
-  #DLURL http://libgeotiff-dfsg.sourcearchive.com/downloads/${VER}/libgeotiff-dfsg_${VER}.orig.tar.gz
+  GIT_TAG xp${GTIFVER} # what to 'git checkout'
+  GIT_REF v${GTIFVER} # create patch from this tag to 'git checkout'
+  #DLURL http://libgeotiff-dfsg.sourcearchive.com/downloads/${GTIFVER}/libgeotiff-dfsg_${GTIFVER}.orig.tar.gz
   #DLMD5 35dca74146d6168bc5adcf3495d7546c
   # NOTE: version 1.2.4 appears to be no longer available to download
   # from sourcearchive.com (26 byte invalid file)
-  DLURL ${REPO}/archive/v${VER}.tar.gz
+  DLURL ${REPO}/archive/v${GTIFVER}.tar.gz
   DLMD5 4bef0cc5f066a5f3c0b2352f39bbf140
-  DLNAME libgeotiff-v${VER}.tar.gz
+  DLNAME libgeotiff-v${GTIFVER}.tar.gz
   PATCH ${PATCH_DIR}/libgeotiff.patch
   DIFF ${REPO}/compare/
   )
@@ -36,11 +36,8 @@ function(build_libgeotiff)
     set(XP_PRO_WX${wxver} ON CACHE BOOL "include wx${wxver}" FORCE)
     xpPatchProject(${PRO_WX${wxver}})
   endif()
-  build_wxv(VER ${wxver} TARGETS wxTgts INCDIR wxInc SRCDIR wxSrc)
-  set(XP_CONFIGURE
-    -DWX_INCLUDE:PATH=${wxInc}
-    -DWX_SOURCE:PATH=${wxSrc}
-    )
+  build_wxv(VER ${wxver} TARGETS wxTgts INCDIR wxInc)
+  set(XP_CONFIGURE -DWX_INCLUDE:PATH=${wxInc} -DGEOTIFF_VER=${GTIFVER})
   configure_file(${PRO_DIR}/use/usexp-geotiff-config.cmake ${STAGE_DIR}/share/cmake/
     @ONLY NEWLINE_STYLE LF
     )
--- projects/use/usexp-geotiff-config.cmake
+++ projects/use/usexp-geotiff-config.cmake
@@ -5,11 +5,13 @@ set(prj geotiff)
 # this file (-config) installed to share/cmake
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
-# targets file (-targets) installed to lib/cmake
-include(${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
 string(TOUPPER ${prj} PRJ)
+set(gtifVer _@GTIFVER@)
+set(gtifVerDir /${prj}${gtifVer})
 unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR geotiff/geotiff.h PATHS ${XP_ROOTDIR}/include NO_DEFAULT_PATH)
+find_path(${PRJ}_INCLUDE_DIR geotiff/geotiff.h PATHS ${XP_ROOTDIR}/include${gtifVerDir} NO_DEFAULT_PATH)
+# targets file (-targets) installed to lib/cmake
+include(${XP_ROOTDIR}/lib/cmake/${prj}${gtifVer}-targets.cmake)
 set(${PRJ}_LIBRARIES geotiff)
 set(reqVars ${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES)
 include(FindPackageHandleStandardArgs)
