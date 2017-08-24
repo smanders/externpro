5afdbdbbff320ae9ff643d1c36d3fb72a839810b
Scott M Anderson <smanders@sdl.usu.edu>
2017-08-24 17:08:16 -0600

    gsoap version libraries and includes
    * embed version in library name, targets file, and include directories
    * see issue https://github.com/smanders/externpro/issues/142

--- projects/use/usexp-gsoap-config.cmake
+++ projects/use/usexp-gsoap-config.cmake
@@ -5,12 +5,14 @@ set(prj gsoap)
 # this file (-config) installed to share/cmake
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
-# targets file (-targets) installed to lib/cmake
-include(${XP_ROOTDIR}/lib/cmake/${prj}-targets.cmake)
 string(TOUPPER ${prj} PRJ)
+set(ver _@GSPVER@)
+set(verDir /${prj}${ver})
 unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR gsoap/stdsoap2.h PATHS ${XP_ROOTDIR}/include NO_DEFAULT_PATH)
-list(APPEND ${PRJ}_INCLUDE_DIR ${XP_ROOTDIR}/include/gsoap) # for internal header includes
+find_path(${PRJ}_INCLUDE_DIR gsoap/stdsoap2.h PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
+list(APPEND ${PRJ}_INCLUDE_DIR ${XP_ROOTDIR}/include${verDir}/gsoap) # for internal header includes
+# targets file (-targets) installed to lib/cmake
+include(${XP_ROOTDIR}/lib/cmake/${prj}${ver}-targets.cmake)
 set(${PRJ}_LIBRARIES gsoap)
 set(reqVars ${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES)
 include(FindPackageHandleStandardArgs)
