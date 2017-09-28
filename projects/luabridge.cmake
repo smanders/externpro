2a58a9225638e8cd4da1b9c54ba9137e74c925ec
Scott M Anderson <smanders@sdl.usu.edu>
2017-09-28 17:31:46 -0600

    LuaBridge patch removed, main repo has fix
    * see issue https://github.com/smanders/externpro/issues/147

--- projects/luabridge.cmake
+++ projects/luabridge.cmake
@@ -1,23 +1,18 @@
 # luabridge
 set(VER 2.0) # repo isn't tagged with version 2.0 - 1.0.2 was the last tag
-set(TAG 04b47d723d35b47ff8efce63d54ef264a59152b5)
-set(TAGDATE 131019) # 2013.10.19 commit, head of master branch on 2014.12.05
-set(REPO https://github.com/smanders/LuaBridge)
+set(TAG 244f9b35498feab3039a58ffb258367d7bc054ac) # 2017.03.31 commit, head of master branch on 2017.09.28
+set(REPO https://github.com/vinniefalco/LuaBridge)
 set(PRO_LUABRIDGE
   NAME luabridge
   SUPERPRO lua
-  WEB "LuaBridge" http://vinniefalco.com/LuaBridge/Manual.html "LuaBridge Reference Manual"
+  WEB "LuaBridge" http://vinniefalco.github.io/LuaBridge/Manual.html "LuaBridge Reference Manual"
   LICENSE "open" "${REPO}#official-repository" "MIT license"
   DESC "a lightweight, dependency-free library for binding Lua to C++"
-  REPO "repo" ${REPO} "forked LuaBridge repo on github"
+  REPO "repo" ${REPO} "LuaBridge repo on github"
   VER ${VER}
-  GIT_ORIGIN git://github.com/smanders/LuaBridge.git
-  GIT_UPSTREAM git://github.com/vinniefalco/LuaBridge.git
-  GIT_TAG xp${TAGDATE}
-  GIT_REF ${TAG}
+  GIT_ORIGIN git://github.com/vinniefalco/LuaBridge.git
+  GIT_TAG ${TAG}
   DLURL ${REPO}/archive/${TAG}.tar.gz
-  DLMD5 54e72d497c898b9c7a133c653878682e
+  DLMD5 0ac0ac198ec37ad5782dd5269ebbbe23
   DLNAME LuaBridge-${VER}.tar.gz
-  PATCH ${PATCH_DIR}/luabridge.patch
-  DIFF ${REPO}/compare/vinniefalco:
   )
