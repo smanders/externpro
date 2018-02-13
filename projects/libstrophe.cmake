ede9c5a819b9f8b24e5bd7edc8d461f0d38eb8da
Scott M Anderson <smanders@sdl.usu.edu>
2018-02-13 16:55:47 -0700

    libstrophe depends on EXPAT

--- projects/libstrophe.cmake
+++ projects/libstrophe.cmake
@@ -24,12 +24,19 @@ function(build_libstrophe)
   if(NOT (XP_DEFAULT OR XP_PRO_LIBSTROPHE))
     return()
   endif()
+  if(NOT (XP_DEFAULT OR XP_PRO_EXPAT))
+    message(STATUS "libstrophe: requires expat")
+    set(XP_PRO_EXPAT ON CACHE BOOL "include expat" FORCE)
+    xpPatchProject(${PRO_EXPAT})
+  endif()
   if(NOT (XP_DEFAULT OR XP_PRO_OPENSSL))
     message(STATUS "libstrophe: requires openssl")
     set(XP_PRO_OPENSSL ON CACHE BOOL "include openssl" FORCE)
     xpPatchProject(${PRO_OPENSSL})
   endif()
+  build_expat(expatTgts)
   build_openssl(osslTgts)
+  set(depTgts ${expatTgts} ${osslTgts})
   xpGetArgValue(${PRO_LIBSTROPHE} ARG VER VALUE VER)
   #configure_file(${PRO_DIR}/use/usexp-libstrophe-config.cmake ${STAGE_DIR}/share/cmake/
   #  @ONLY NEWLINE_STYLE LF
@@ -37,6 +44,7 @@ function(build_libstrophe)
   set(XP_CONFIGURE
     -DLIBSTROPHE_VER=${VER}
     -DCMAKE_USE_OPENSSL_MODULE_PATH=ON
+    -DCMAKE_USE_EXPAT_MODULE_PATH=ON
     )
-  xpCmakeBuild(libstrophe "${osslTgts}" "${XP_CONFIGURE}")
+  xpCmakeBuild(libstrophe "${depTgts}" "${XP_CONFIGURE}")
 endfunction()
