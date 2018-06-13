0072ddda571a920c03dec6d2b067914c52b42d24
Scott M Anderson <smanders@sdl.usu.edu>
2018-06-13 10:10:00 -0600

    wxwidgets use script IMPORTED targets
    issue https://github.com/smanders/externpro/issues/197#issuecomment-396744290
    * wxcmakeover for MSW build (wxcmake31.patch), including NAMESPACE wx:: (which also effects non-MSW)
    * set_target_properties INTERFACE_LINK_LIBRARIES to build up wx library dependency graph, including gtk and system libs
    
    issue https://github.com/smanders/externpro/issues/194
    * no longer using wxWidgets_LIBRARIES from find_package/FindwxWidgets.cmake
    * using wx::<C> targets from add_library with IMPORTED and IMPORTED_LOCATION
    
    issue https://github.com/smanders/externpro/issues/183#issuecomment-383146697
    * removed include of the wxWidgets use file
    * there is still a question of whether wxWidgets_CXX_FLAGS (-pthread on linux) is needed

--- projects/use/usexp-wxwidgets-config.cmake
+++ projects/use/usexp-wxwidgets-config.cmake
@@ -21,51 +21,135 @@ endif()
 # to account for how the targets.cmake files are named
 # this will be used by the wxx use script, too
 string(REGEX REPLACE "([0-9])\\.([0-9])?" "\\1\\2" ${PRJ}_VER ${wxVersion})
-# NOTE: geotiff needs to find tiff, and we use the tiff bundled with wx
-list(APPEND ${PRJ}_INCLUDE_DIR ${XP_ROOTDIR}/include/wx-${wxVersion}/wx/tiff)
+# http://docs.wxwidgets.org/trunk/page_libs.html
+# TRICKY: reverse dependency order (base should be last)
+set(wx_all_libs aui propgrid richtext adv gl html core net xml base)
 if(NOT DEFINED wx_libs)
-  # http://docs.wxwidgets.org/trunk/page_libs.html
-  # TRICKY: reverse dependency order (base should be last)
-  set(wx_libs aui propgrid richtext adv gl html core net xml base)
+  set(wx_libs ${wx_all_libs})
 endif()
+unset(wxLibs)
+foreach(lib ${wx_libs})
+  list(APPEND wxLibs "wx::${lib}") # prepend NAMESPACE from install(EXPORT
+endforeach()
+set(${PRJ}_LIBRARIES ${wxLibs})
+# NOTE: geotiff needs to find tiff, and we use the tiff bundled with wx
+set(${PRJ}_INCLUDE_DIR ${XP_ROOTDIR}/include/wx-${wxVersion}/wx/tiff)
 if(UNIX)
-  set(wxWidgets_CONFIG_OPTIONS --prefix=${XP_ROOTDIR} --version=${wxVersion})
-  unset(_filename CACHE) # temp variable in FindwxWidgets.cmake script?
+  unset(_filename CACHE) # temp variable in FindwxWidgets.cmake script
   unset(wxWidgets_CONFIG_EXECUTABLE CACHE)
   set(wxWidgets_CONFIG_EXECUTABLE ${XP_ROOTDIR}/bin/wx-config)
+  set(wxWidgets_CONFIG_OPTIONS --prefix=${XP_ROOTDIR} --version=${wxVersion})
   find_package(wxWidgets REQUIRED ${wx_libs})
-  include(${wxWidgets_USE_FILE})
-  #set(wx_DEBUG TRUE)
-  if(wx_DEBUG)
-    message(STATUS "wxWidgets_USE_FILE: ${wxWidgets_USE_FILE}")
+  if(XP_WXDEBUG)
     message(STATUS "wxWidgets_INCLUDE_DIRS: ${wxWidgets_INCLUDE_DIRS}")
-    message(STATUS "wxWidgets_LIBRARIES: ${wxWidgets_LIBRARIES}")
-    message(STATUS "wxWidgets_LIBRARY_DIRS: ${wxWidgets_LIBRARY_DIRS}")
+    #message(STATUS "wxWidgets_LIBRARIES: ${wxWidgets_LIBRARIES}")
+    #message(STATUS "wxWidgets_LIBRARY_DIRS: ${wxWidgets_LIBRARY_DIRS}")
     message(STATUS "wxWidgets_DEFINITIONS: ${wxWidgets_DEFINITIONS}")
     message(STATUS "wxWidgets_CXX_FLAGS: ${wxWidgets_CXX_FLAGS}")
+    # -pthread # TODO from wxWidgets_CXX_FLAGS include(${wxWidgets_USE_FILE})
   endif()
   mark_as_advanced(wxWidgets_USE_DEBUG)
   mark_as_advanced(wxWidgets_wxrc_EXECUTABLE)
-  mark_as_advanced(_filename) # temp variable in FindwxWidgets.cmake script?
-  list(APPEND ${PRJ}_INCLUDE_DIR ${wxWidgets_INCLUDE_DIRS})
-  set(${PRJ}_LIBRARIES ${wxWidgets_LIBRARIES})
+  mark_as_advanced(_filename)
   set(${PRJ}_DEFINITIONS ${wxWidgets_DEFINITIONS})
+  list(APPEND ${PRJ}_INCLUDE_DIR ${wxWidgets_INCLUDE_DIRS})
+  ##################
+  include(CheckLibraryExists)
+  function(checkLibraryConcat lib symbol liblist)
+    string(TOUPPER ${lib} LIB)
+    check_library_exists("${lib}" "${symbol}" "" XP_WX_HAS_${LIB})
+    if(XP_WX_HAS_${LIB})
+      list(APPEND ${liblist} ${lib})
+      set(${liblist} ${${liblist}} PARENT_SCOPE)
+    endif()
+  endfunction()
+  ########
+  function(getLibname target _ret)
+    set(unicode u)
+    if(${target} STREQUAL "base")
+      set(${_ret} wx_${target}${unicode}-${wxVersion} PARENT_SCOPE)
+    elseif(${target} STREQUAL "net" OR ${target} STREQUAL "xml")
+      set(${_ret} wx_base${unicode}_${target}-${wxVersion} PARENT_SCOPE)
+    elseif(${target} MATCHES "^wx") # any target that starts with "wx"
+      if(NOT ${target} STREQUAL "wxregex")
+        unset(unicode)
+      endif()
+      set(${_ret} ${target}${unicode}-${wxVersion} PARENT_SCOPE)
+    else()
+      set(${_ret} ${wxbasename}_${target}-${wxVersion} PARENT_SCOPE)
+    endif()
+  endfunction()
+  ##################
   execute_process(COMMAND sh "${wxWidgets_CONFIG_EXECUTABLE}" --prefix=${XP_ROOTDIR}
-    --version=${wxVersion} --version
-    OUTPUT_VARIABLE wxver OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET
+    --version=${wxVersion} --basename --debug=no
+    OUTPUT_VARIABLE wxbasename OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET
     )
-  if(wx_DEBUG)
-    message(STATUS "wxWidgets version: ${wxver}")
-  endif()
-  ############################
-  # GTK
-  if(NOT ${CMAKE_SYSTEM_NAME} STREQUAL Darwin)
+  if(wxbasename MATCHES wx_gtk)
     find_package(PkgConfig)
     pkg_check_modules(GTK REQUIRED gtk+-@GTK_VER@.0)
     if(GTK_FOUND)
       list(APPEND ${PRJ}_INCLUDE_DIR ${GTK_INCLUDE_DIRS})
+      set(_wx_core_libs ${GTK_LIBRARIES})
     endif()
-  endif() # NOT Darwin
+    checkLibraryConcat(X11 XGetWindowAttributes _wx_core_libs)
+    checkLibraryConcat(Xxf86vm XF86VidModeGetAllModeLines _wx_core_libs)
+    checkLibraryConcat(SM SmcOpenConnection _wx_core_libs)
+    # -lgthread-2.0 # TODO: determine if needed and detect appropriately
+  endif()
+  set(_wx_adv_deps core)
+  set(_wx_aui_deps adv html)
+  set(_wx_base_link wxregex wxzlib)
+  set(_wx_core_deps base)
+  set(_wx_core_link wxjpeg wxpng wxtiff)
+  set(_wx_gl_deps core)
+  #set(_wx_gl_libs GL GLU) # TODO determine if needed
+  set(_wx_html_deps core)
+  set(_wx_net_deps base)
+  set(_wx_propgrid_deps adv)
+  set(_wx_richtext_deps adv html xml)
+  set(_wx_wxpng_link wxzlib)
+  set(_wx_wxtiff_link wxjpeg wxzlib)
+  set(_wx_xml_deps base)
+  set(_wx_xml_link wxexpat)
+  checkLibraryConcat(dl dlclose _wx_base_libs)
+  checkLibraryConcat(m pow _wx_base_libs)
+  # standard way is to get these _wx_link libs from 'wx-config --libs'
+  set(_wx_link wxexpat wxjpeg wxpng wxregex wxscintilla wxtiff wxzlib)
+  foreach(lib ${wx_all_libs} ${_wx_link})
+    add_library(wx::${lib} STATIC IMPORTED)
+    getLibname(${lib} ${lib}filename)
+    set(${lib}_RELEASE ${XP_ROOTDIR}/lib/lib${${lib}filename}.a)
+    if(EXISTS "${${lib}_RELEASE}")
+      set_property(TARGET wx::${lib} APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+      set_target_properties(wx::${lib} PROPERTIES
+        IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+        IMPORTED_LOCATION_RELEASE "${${lib}_RELEASE}"
+        )
+      if(_wx_${lib}_deps OR _wx_${lib}_link OR _wx_${lib}_libs)
+        unset(linkLibs)
+        foreach(dep ${_wx_${lib}_deps})
+          list(APPEND linkLibs wx::${dep})
+        endforeach()
+        foreach(dep ${_wx_${lib}_link})
+          list(APPEND linkLibs \$<LINK_ONLY:wx::${dep}>)
+        endforeach()
+        foreach(dep ${_wx_${lib}_libs})
+          list(APPEND linkLibs \$<LINK_ONLY:${dep}>)
+        endforeach()
+        set_target_properties(wx::${lib} PROPERTIES
+          INTERFACE_LINK_LIBRARIES "${linkLibs}"
+          )
+      endif()
+    endif()
+  endforeach()
+  execute_process(COMMAND sh "${wxWidgets_CONFIG_EXECUTABLE}" --prefix=${XP_ROOTDIR}
+    --version=${wxVersion} --version
+    OUTPUT_VARIABLE wxver OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET
+    )
+  if(XP_WXDEBUG)
+    message(STATUS "wxWidgets version: ${wxver}")
+    message(STATUS "wxWidgets LIBRARIES: ${${PRJ}_LIBRARIES}")
+  endif()
 elseif(MSVC)
   add_definitions(-DwxUSE_NO_MANIFEST)
   set(${PRJ}_DEFINITIONS -DwxUSE_NO_MANIFEST)
@@ -75,14 +159,12 @@ elseif(MSVC)
     )
   # targets file (-targets) installed to lib/cmake
   include(${XP_ROOTDIR}/lib/cmake/${prj}${${PRJ}_VER}-targets.cmake)
-  set(${PRJ}_LIBRARIES ${wx_libs}
-    # on unix these libs come from wx-config --libs
-    wxexpat wxjpeg wxpng wxregex wxscintilla wxtiff wxzlib
+  list(APPEND ${PRJ}_LIBRARIES
     rpcrt4 comctl32 # system libraries required by wxCore
     gdiplus # wxUSE_GRAPHICS_CONTEXT set to 1 in setup.h
     )
 endif()
-set(reqVars ${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES ${PRJ}_VER ${PRJ}_DEFINITIONS)
+set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES ${PRJ}_DEFINITIONS)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
 mark_as_advanced(${reqVars})
