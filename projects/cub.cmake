82ae8c0b18ca245ff66cfebb31eb9b69ca1d3d18
Scott M Anderson <smanders@sdl.usu.edu>
2018-09-24 10:10:34 -0600

    add CUB version 1.8.0
    issue https://github.com/smanders/externpro/issues/215

---
+++ projects/cub.cmake
@@ -0,0 +1,39 @@
+# cub
+xpProOption(cub)
+set(VER 1.8.0)
+set(REPO https://github.com/NVlabs/cub)
+set(PRO_CUB
+  NAME cub
+  WEB "CUB" http://nvlabs.github.io/cub/ "CUB Project Website"
+  LICENSE "open" "${REPO}/tree/v${VER}#open-source-license" "BSD 3-Clause"
+  DESC "flexible library of cooperative threadblock primitives and other utilites for CUDA kernel programming"
+  REPO "repo" ${REPO} "CUB repo on github"
+  VER ${VER}
+  GIT_ORIGIN git://github.com/NVlabs/cub.git
+  GIT_TAG v${VER} # what to 'git checkout'
+  GIT_REF v${VER} # create patch from this tag to 'git checkout'
+  DLURL ${REPO}/archive/v${VER}.tar.gz
+  DLMD5 9203ea2499b56782601fddf8a12e9b08
+  DLNAME cub-v${VER}.tar.gz
+  )
+########################################
+function(build_cub)
+  if(NOT (XP_DEFAULT OR XP_PRO_CUB))
+    return()
+  endif()
+  xpGetArgValue(${PRO_CUB} ARG VER VALUE VER)
+  set(verDir /cub_${VER})
+  configure_file(${PRO_DIR}/use/usexp-cub-config.cmake ${STAGE_DIR}/share/cmake/
+    @ONLY NEWLINE_STYLE LF
+    )
+  ExternalProject_Get_Property(cub SOURCE_DIR)
+  ExternalProject_Add(cub_bld DEPENDS cub
+    DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR} CONFIGURE_COMMAND ""
+    SOURCE_DIR ${SOURCE_DIR} BINARY_DIR ${NULL_DIR} INSTALL_DIR ${NULL_DIR}
+    BUILD_COMMAND ${CMAKE_COMMAND} -E copy_directory
+      <SOURCE_DIR>/cub ${STAGE_DIR}/include${verDir}/cub
+    INSTALL_COMMAND ""
+    )
+  set_property(TARGET cub_bld PROPERTY FOLDER ${bld_folder})
+  message(STATUS "target cub_bld")
+endfunction()
---
+++ projects/use/usexp-cub-config.cmake
@@ -0,0 +1,16 @@
+# CUB_FOUND - cub was found
+# CUB_VER - cub version
+# CUB_INCLUDE_DIR - the cub include directory
+set(prj cub)
+# this file (-config) installed to share/cmake
+get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
+get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
+string(TOUPPER ${prj} PRJ)
+set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
+set(verDir /${prj}_@VER@)
+unset(${PRJ}_INCLUDE_DIR CACHE)
+find_path(${PRJ}_INCLUDE_DIR cub/cub.cuh PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
+set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
+mark_as_advanced(${reqVars})
