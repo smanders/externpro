7b183f441ec42859433ed738717fe46efa33cf86
Scott M Anderson <smanders@sdl.usu.edu>
2019-06-13 18:18:03 -0600

    node projects: simplify urls
    issue https://github.com/smanders/externpro/issues/219

--- projects/node_10.16.0.cmake
+++ projects/node_10.16.0.cmake
@@ -1,15 +1,15 @@
 set(VER 10.16.0)
 xpProOption(node_${VER})
-set(REPO https://github.com/nodejs/node)
+set(REPO github.com/nodejs/node)
 set(PRO_NODE_${VER}
   NAME node_${VER}
   WEB "Node.js" http://nodejs.org "Node.js website"
   LICENSE "open" https://raw.githubusercontent.com/nodejs/node/v${VER}/LICENSE "MIT license"
   DESC "platform to build scalable network applications"
-  REPO "repo" ${REPO} "node repo on github"
+  REPO "repo" https://${REPO} "node repo on github"
   GRAPH GRAPH_NODE nodejs GRAPH_DEPS nasm
   VER ${VER}
-  GIT_ORIGIN git://github.com/nodejs/node.git
+  GIT_ORIGIN git://${REPO}.git
   GIT_TAG v${VER} # what to 'git checkout'
   DLURL http://nodejs.org/dist/v${VER}/node-v${VER}.tar.gz
   DLMD5 46f47630e088540968962f1ad71b9d0a
--- projects/node_8.12.0.cmake
+++ projects/node_8.12.0.cmake
@@ -1,14 +1,14 @@
 set(VER 8.12.0)
 xpProOption(node_${VER})
-set(REPO https://github.com/nodejs/node)
+set(REPO github.com/nodejs/node)
 set(PRO_NODE_${VER}
   NAME node_${VER}
   WEB "Node.js" http://nodejs.org "Node.js website"
   LICENSE "open" https://raw.githubusercontent.com/nodejs/node/v${VER}/LICENSE "MIT license"
   DESC "platform to build scalable network applications"
-  REPO "repo" ${REPO} "node repo on github"
+  REPO "repo" https://${REPO} "node repo on github"
   VER ${VER}
-  GIT_ORIGIN git://github.com/nodejs/node.git
+  GIT_ORIGIN git://${REPO}.git
   GIT_TAG v${VER} # what to 'git checkout'
   DLURL http://nodejs.org/dist/v${VER}/node-v${VER}.tar.gz
   DLMD5 5690333b77964edf81945fc724f6ea85
