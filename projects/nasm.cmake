5ea8c7aa2617097eb926b4ac4e7d4cf50cf4702f
Scott M Anderson <smanders@sdl.usu.edu>
2019-06-14 11:29:41 -0600

    download and extract nasm zip, not installer
    * node will find nasm executable in SOURCE_DIR (location of extracted zip)
    * no need for patch_nasm, find_nasm, or build_nasm cmake functions
    * issue https://github.com/smanders/externpro/issues/240

--- projects/nasm.cmake
+++ projects/nasm.cmake
@@ -9,32 +9,6 @@ set(PRO_NASM
   DESC "The Netwide Assembler - an 80x86 and x86-64 assembler"
   GRAPH GRAPH_SHAPE box
   VER ${VER}
-  DLURL ${URL}/pub/nasm/releasebuilds/${VER}/win64/nasm-${VER}-installer-x64.exe
-  DLMD5 e871a6d2651e81ce8674ae2d31fe33b0
+  DLURL ${URL}/pub/nasm/releasebuilds/${VER}/win64/nasm-${VER}-win64.zip
+  DLMD5 bce3b7e205bab546ead85db761df89db
   )
-########################################
-function(patch_nasm)
-  # don't want xpPatch(${PRO_NASM}) called automatically
-  # because then an ExternalProject_Add(nasm) will try to extract the executable
-  # which doesn't make any sense, since this executable is an installer
-endfunction()
-########################################
-function(find_nasm)
-  find_program(NASM_EXECUTABLE)
-  if(NASM_EXECUTABLE)
-    execute_process(COMMAND ${NASM_EXECUTABLE} -version
-      OUTPUT_VARIABLE nasmVersion
-      ERROR_QUIET
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-    if(nasmVersion MATCHES "^NASM version ([0-9\\.]*)")
-      set(NASM_VERSION_STRING "${CMAKE_MATCH_1}")
-    endif()
-    unset(nasmVersion)
-  endif()
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(nasm
-    REQUIRED_VARS NASM_EXECUTABLE
-    VERSION_VAR NASM_VERSION_STRING
-    )
-endfunction()
