336063e0637910220f976c0ff15172dc0654c865
Scott M Anderson <smanders@sdl.usu.edu>
2019-06-14 11:28:06 -0600

    patch node to find nasm
    issue https://github.com/smanders/externpro/issues/219

--- projects/node_10.16.0.cmake
+++ projects/node_10.16.0.cmake
@@ -1,6 +1,7 @@
 set(VER 10.16.0)
 xpProOption(node_${VER})
 set(REPO github.com/nodejs/node)
+set(FORK github.com/smanders/node)
 set(PRO_NODE_${VER}
   NAME node_${VER}
   WEB "Node.js" http://nodejs.org "Node.js website"
@@ -9,8 +10,12 @@ set(PRO_NODE_${VER}
   REPO "repo" https://${REPO} "node repo on github"
   GRAPH GRAPH_NODE nodejs GRAPH_DEPS nasm
   VER ${VER}
-  GIT_ORIGIN git://${REPO}.git
-  GIT_TAG v${VER} # what to 'git checkout'
+  GIT_ORIGIN git://${FORK}.git
+  GIT_UPSTREAM git://${REPO}.git
+  GIT_TAG xp${VER} # what to 'git checkout'
+  GIT_REF v${VER} # create patch from this tag to 'git checkout'
+  PATCH ${PATCH_DIR}/node_${VER}.patch
+  DIFF ${FORK}/compare/node:
   DLURL http://nodejs.org/dist/v${VER}/node-v${VER}.tar.gz
   DLMD5 46f47630e088540968962f1ad71b9d0a
   )
