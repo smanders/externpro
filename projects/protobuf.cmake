be7ffd9ebfe4b2d3661e68673490b990e4b7b039
Scott M Anderson <smanders@sdl.usu.edu>
2019-11-11 10:56:25 -0700

    protobuf enhancements
    * use XP_NAMESPACE (xpro::)
    * update upstream URL (protocolbuffers now, not google)
    * copy xpro::libprotobuf properties to libprotobuf
      * support current internpro release (palam 1.5.0.0) which references libprotobuf in palam::mb targets file
    * PROTOBUF_INCLUDE_DIR used by protobuf-module.cmake (for PROTOBUF_GNERATE_CPP)
    * issue https://github.com/smanders/externpro/issues/244

--- projects/protobuf.cmake
+++ projects/protobuf.cmake
@@ -2,7 +2,7 @@
 xpProOption(protobuf DBG)
 set(VER 3.0.0-beta-1)
 set(REPO https://github.com/smanders/protobuf)
-set(REPO_UPSTREAM https://github.com/google/protobuf)
+set(REPO_UPSTREAM https://github.com/protocolbuffers/protobuf)
 set(PRO_PROTOBUF
   NAME protobuf
   WEB "protobuf" https://developers.google.com/protocol-buffers/ "Protocol Buffers website"
@@ -12,14 +12,14 @@ set(PRO_PROTOBUF
   GRAPH BUILD_DEPS zlib
   VER ${VER}
   GIT_ORIGIN git://github.com/smanders/protobuf.git
-  GIT_UPSTREAM git://github.com/google/protobuf.git
+  GIT_UPSTREAM git://github.com/protocolbuffers/protobuf.git
   GIT_TAG xp${VER} # what to 'git checkout'
   GIT_REF v${VER} # create patch from this tag to 'git checkout'
   DLURL ${REPO_UPSTREAM}/archive/v${VER}.tar.gz
   DLMD5 63aad3f1814b5c6cd06c7712cd5ba9db
   DLNAME protobuf-${VER}.tar.gz
   PATCH ${PATCH_DIR}/protobuf.patch
-  DIFF ${REPO}/compare/google:
+  DIFF ${REPO}/compare/protocolbuffers:
   )
 ########################################
 function(build_protobuf)
@@ -35,6 +35,7 @@ function(build_protobuf)
     -DBUILD_TESTING=OFF # we don't have gmock, unless we switch to a release tar ball
     -DZLIB_MODULE_PATH=ON # with this option ON, we don't need -DZLIB=ON
     -DCMAKE_INSTALL_LIBDIR=lib # without this *some* platforms (RHEL, but not Ubuntu) install to lib64
+    -DXP_NAMESPACE:STRING=xpro
     -DPROTOBUF_VER=${VER}
     )
   xpCmakeBuild(protobuf "${depTgts}" "${XP_CONFIGURE}")
--- projects/use/usexp-protobuf-config.cmake
+++ projects/use/usexp-protobuf-config.cmake
@@ -12,14 +12,29 @@ get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
 string(TOUPPER ${prj} PRJ)
 set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
-set(ver _@VER@)
-set(verDir /${prj}${ver})
-unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR google/protobuf/service.h PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
+set(nsPrefix xpro::) # TRICKY: nsPrefix also used in protobuf-module.cmake
 # protobuf installs a config file which includes -targets.cmake and -module.cmake
-include(${XP_ROOTDIR}/lib/cmake/protobuf${ver}/${prj}-config.cmake)
-set(${PRJ}_LIBRARIES libprotobuf)
-set(${PRJ}_PROTOC_EXECUTABLE protoc) # TRICKY: must be named this to match what's used in -module.cmake
+include(${XP_ROOTDIR}/lib/cmake/protobuf_@VER@/${prj}-config.cmake)
+set(${PRJ}_LIBRARIES ${nsPrefix}libprotobuf)
+if(NOT TARGET libprotobuf)
+  add_library(libprotobuf STATIC IMPORTED)
+  foreach(prop
+    INTERFACE_INCLUDE_DIRECTORIES
+    IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE
+    IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG
+    IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE
+    IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG
+    IMPORTED_LOCATION_RELEASE
+    IMPORTED_LOCATION_DEBUG
+    )
+    get_target_property(tmp ${nsPrefix}libprotobuf ${prop})
+    if(tmp)
+      set_target_properties(libprotobuf PROPERTIES ${prop} "${tmp}")
+    endif()
+  endforeach()
+endif()
+set(${PRJ}_PROTOC_EXECUTABLE ${nsPrefix}protoc) # TRICKY: must be named to match what's used in -module.cmake
+get_target_property(PROTOBUF_INCLUDE_DIR ${nsPrefix}libprotobuf INTERFACE_INCLUDE_DIRECTORIES)
 set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES ${PRJ}_PROTOC_EXECUTABLE)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
