fac16414531afc816549dd3a2939544f62c8e367
Scott M Anderson <smanders@sdl.usu.edu>
2020-02-04 18:27:39 -0700

    fork from official jasper github repo
    issue https://github.com/smanders/externpro/issues/259

--- projects/jasper.cmake
+++ projects/jasper.cmake
@@ -1,23 +1,23 @@
 # jasper
-# http://packages.debian.org/sid/libjasper-dev
-# http://jasper.sourcearchive.com/
 xpProOption(jasper DBG)
 set(VER 1.900.1)
-set(REPO github.com/smanders/jasper)
+set(REPO github.com/mdadams/jasper)
+set(FORK github.com/smanders/jasper)
 set(PRO_JASPER
   NAME jasper
   WEB "JasPer" http://www.ece.uvic.ca/~frodo/jasper/ "JasPer website"
   LICENSE "open" "http://www.ece.uvic.ca/~frodo/jasper/#license" "JasPer License (based on MIT license)"
   DESC "JPEG 2000 Part-1 codec implementation"
-  REPO "repo" https://${REPO} "jasper repo on github"
+  REPO "repo" https://${FORK} "forked jasper repo on github"
   VER ${VER}
-  GIT_ORIGIN git://${REPO}.git
-  GIT_TAG xpp${VER} # what to 'git checkout'
-  GIT_REF jv${VER} # create patch from this tag to 'git checkout'
+  GIT_UPSTREAM git://${REPO}.git
+  GIT_ORIGIN git://${FORK}.git
+  GIT_TAG xp-${VER} # what to 'git checkout'
+  GIT_REF version-${VER} # create patch from this tag to 'git checkout'
   DLURL http://www.ece.uvic.ca/~frodo/jasper/software/jasper-${VER}.zip
   DLMD5 a342b2b4495b3e1394e161eb5d85d754
   PATCH ${PATCH_DIR}/jasper.patch
-  DIFF https://${REPO}/compare/
+  DIFF https://${FORK}/compare/
   )
 ########################################
 function(build_jasper)
