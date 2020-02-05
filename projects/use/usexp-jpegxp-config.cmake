27cb944325b05c878046594a652c8dd3c7c1595d
Scott M Anderson <smanders@sdl.usu.edu>
2020-02-05 12:18:06 -0700

    simplify jpegxp use script
    * jpegxp commit https://github.com/smanders/jpegxp/commit/e65c8e4e35cdf8798a0921a91fe17ee515f4d9b9
    * use XP_NAMESPACE (xpro::)
    * INSTALL_INTERFACE for include directory
    * issue https://github.com/smanders/externpro/issues/264

--- projects/use/usexp-jpegxp-config.cmake
+++ projects/use/usexp-jpegxp-config.cmake
@@ -1,21 +1,16 @@
 # JPEGXP_FOUND - jpegxp was found
 # JPEGXP_VER - jpegxp version
-# JPEGXP_INCLUDE_DIR - the jpegxp include directory
-# JPEGXP_LIBRARIES - the jpegxp libraries
+# JPEGXP_LIBRARIES - the jpegxp library
 set(prj jpegxp)
 # this file (-config) installed to share/cmake
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
 string(TOUPPER ${prj} PRJ)
 set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
-set(ver _@VER@)
-set(verDir /${prj}${ver})
-unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR jpegxp/jpeglib.h PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
 # targets file (-targets) installed to lib/cmake
-include(${XP_ROOTDIR}/lib/cmake/${prj}${ver}-targets.cmake)
-set(${PRJ}_LIBRARIES jpegxp)
-set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES)
+include(${XP_ROOTDIR}/lib/cmake/${prj}_@VER@-targets.cmake)
+set(${PRJ}_LIBRARIES xpro::jpegxp)
+set(reqVars ${PRJ}_VER ${PRJ}_LIBRARIES)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
 mark_as_advanced(${reqVars})
