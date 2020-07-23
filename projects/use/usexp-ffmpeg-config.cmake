94e2dd8f858c3ab044e9e90f88a54dea72d6951b
Scott M Anderson <smanders@sdl.usu.edu>
2020-07-23 16:05:11 -0600

    enhance ffmpeg use script
    add_library() for ffmpeg libraries (STATIC IMPORTED)
    bundle include directories, link libraries (including dependencies)
    issue https://github.com/smanders/externpro/issues/273

--- projects/use/usexp-ffmpeg-config.cmake
+++ projects/use/usexp-ffmpeg-config.cmake
@@ -1,10 +1,7 @@
 # FFMPEG_FOUND - FFmpeg was found
 # FFMPEG_VER - FFmpeg version
-# FFMPEG_INCLUDE_DIR - the FFmpeg include directories
 # FFMPEG_LIBRARIES - the FFmpeg libraries
 # FFMPEG_DLLS - the FFmpeg shared objects (dll, so)
-xpFindPkg(PKGS openh264) # dependencies
-xpGetPkgVar(openh264 LIBRARIES) # sets OPENH264_LIBRARIES
 set(prj ffmpeg)
 # this file (-config) installed to share/cmake
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
@@ -13,21 +10,70 @@ string(TOUPPER ${prj} PRJ)
 set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
 set(ver _@VER@)
 set(verDir /${prj}${ver})
-unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR ffmpeg/libavcodec/avcodec.h PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
-list(APPEND ${PRJ}_INCLUDE_DIR ${XP_ROOTDIR}/include${verDir}/ffmpeg) # for internal header includes
-set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR)
-set(${PRJ}_LIBRARIES
-  avdevice${ver}
-  avformat${ver}
-  avfilter${ver}
-  avcodec${ver}
-  swresample${ver}
-  swscale${ver}
-  avutil${ver}
-  )
-list(APPEND reqVars ${PRJ}_LIBRARIES)
-link_directories(${XP_ROOTDIR}/lib)
+set(includeDirs ${XP_ROOTDIR}/include${verDir} ${XP_ROOTDIR}/include${verDir}/ffmpeg)
+# libraries, in linking order
+# https://github.com/FFmpeg/FFmpeg/blob/n2.6.2/configure#L2667-L2675
+set(ffmpeg_all_libs avdevice avfilter avformat avcodec swresample swscale avutil)
+if(NOT DEFINED ffmpeg_libs)
+  set(ffmpeg_libs ${ffmpeg_all_libs})
+endif()
+include(CheckLibraryExists)
+function(checkLibraryConcat lib symbol liblist)
+  string(TOUPPER ${lib} LIB)
+  check_library_exists("${lib}" "${symbol}" "" XP_FFMPEG_HAS_${LIB})
+  if(XP_FFMPEG_HAS_${LIB})
+    list(APPEND ${liblist} ${lib})
+    set(${liblist} ${${liblist}} PARENT_SCOPE)
+  endif()
+endfunction()
+# _ffmpeg_*_libs
+checkLibraryConcat(asound snd_strerror _ffmpeg_avdevice_libs)
+checkLibraryConcat(Xext XShmDetach _ffmpeg_avdevice_libs)
+xpFindPkg(PKGS openh264)
+xpGetPkgVar(openh264 LIBRARIES) # sets OPENH264_LIBRARIES
+set(_ffmpeg_avcodec_libs ${OPENH264_LIBRARIES})
+# _ffmpeg_*_deps
+set(_ffmpeg_avdevice_deps avformat)
+set(_ffmpeg_avfilter_deps swresample swscale) # libavfilter code calls swr_*, sws_* functions
+set(_ffmpeg_avformat_deps avcodec)
+set(_ffmpeg_avcodec_deps swresample) # libavcodec code calls swr_* functions
+set(_ffmpeg_swresample_deps avutil)
+set(_ffmpeg_swscale_deps avutil)
+set(_ffmpeg_avutil_deps)
+foreach(lib ${ffmpeg_all_libs})
+  if(NOT TARGET ffmpeg::${lib})
+    add_library(ffmpeg::${lib} STATIC IMPORTED)
+    set(${lib}_RELEASE ${XP_ROOTDIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${ver}${CMAKE_STATIC_LIBRARY_SUFFIX})
+    if(EXISTS "${${lib}_RELEASE}")
+      set_property(TARGET ffmpeg::${lib} APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+      set_target_properties(ffmpeg::${lib} PROPERTIES
+        IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "ASM_NASM;C;CXX"
+        IMPORTED_LOCATION_RELEASE "${${lib}_RELEASE}"
+        )
+      set_target_properties(ffmpeg::${lib} PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${includeDirs}"
+        )
+      if(_ffmpeg_${lib}_deps OR _ffmpeg_${lib}_libs)
+        unset(linkLibs)
+        foreach(dep ${_ffmpeg_${lib}_deps})
+          list(APPEND linkLibs \$<LINK_ONLY:ffmpeg::${dep}>)
+        endforeach()
+        foreach(dep ${_ffmpeg_${lib}_libs})
+          list(APPEND linkLibs \$<LINK_ONLY:${dep}>)
+        endforeach()
+        set_target_properties(ffmpeg::${lib} PROPERTIES
+          INTERFACE_LINK_LIBRARIES "${linkLibs}"
+          )
+      endif()
+    endif()
+  endif()
+endforeach()
+unset(ffmpegLibs)
+foreach(lib ${ffmpeg_libs})
+  list(APPEND ffmpegLibs "ffmpeg::${lib}") # prepend NAMESPACE
+endforeach()
+set(${PRJ}_LIBRARIES ${ffmpegLibs})
+set(reqVars ${PRJ}_VER ${PRJ}_LIBRARIES)
 if(WIN32)
   set(${PRJ}_DLLS
     ${XP_ROOTDIR}/bin${verDir}/avcodec-56.dll
@@ -39,19 +85,6 @@ if(WIN32)
     ${XP_ROOTDIR}/bin${verDir}/swscale-3.dll
     )
   list(APPEND reqVars ${PRJ}_DLLS)
-else()
-  include(CheckLibraryExists)
-  function(checkLibraryConcat lib symbol liblist)
-    string(TOUPPER ${lib} LIB)
-    check_library_exists("${lib}" "${symbol}" "" XP_FFMPEG_HAS_${LIB})
-    if(XP_FFMPEG_HAS_${LIB})
-      list(APPEND ${liblist} ${lib})
-      set(${liblist} ${${liblist}} PARENT_SCOPE)
-    endif()
-  endfunction()
-  checkLibraryConcat(asound snd_strerror syslibs)
-  checkLibraryConcat(Xext XShmDetach syslibs)
-  list(APPEND ${PRJ}_LIBRARIES ${OPENH264_LIBRARIES} ${syslibs})
 endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
