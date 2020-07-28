30cdbd4f8f79ca931d5acd8f1efff0b46115af71
Scott M Anderson <smanders@sdl.usu.edu>
2020-07-28 12:47:31 -0600

    simplify use script: remove RAPIDXML_INCLUDE_DIR
    issue https://github.com/smanders/externpro/issues/273

--- projects/use/usexp-rapidxml-config.cmake
+++ projects/use/usexp-rapidxml-config.cmake
@@ -1,24 +1,18 @@
 # RAPIDXML_FOUND - RapidXml was found
 # RAPIDXML_VER - RapidXml version
-# RAPIDXML_INCLUDE_DIR - the RapidXml include directory
 set(prj rapidxml)
 # this file (-config) installed to share/cmake
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
 string(TOUPPER ${prj} PRJ)
 set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
-set(verDir /${prj}_@VER@)
-unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR rapidxml/rapidxml.hpp PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
 if(NOT TARGET xpro::rapidxml)
   add_library(xpro::rapidxml INTERFACE IMPORTED)
-  if(${PRJ}_INCLUDE_DIR)
-    set_target_properties(xpro::rapidxml PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${${PRJ}_INCLUDE_DIR}"
-      )
-  endif()
+  set_target_properties(xpro::rapidxml PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${XP_ROOTDIR}/include/${prj}_@VER@"
+    )
 endif()
-set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR)
+set(reqVars ${PRJ}_VER)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
 mark_as_advanced(${reqVars})
