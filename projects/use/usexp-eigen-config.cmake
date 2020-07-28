cffd47593e1b6f378f033dd23dde373b5a1d14b6
Scott M Anderson <smanders@sdl.usu.edu>
2020-07-28 12:42:16 -0600

    simplify use script: remove EIGEN_INCLUDE_DIR
    issue https://github.com/smanders/externpro/issues/273

--- projects/use/usexp-eigen-config.cmake
+++ projects/use/usexp-eigen-config.cmake
@@ -1,6 +1,5 @@
 # EIGEN_FOUND - Eigen was found
 # EIGEN_VER - Eigen version
-# EIGEN_INCLUDE_DIR - the Eigen include directory
 set(prj eigen)
 # this file (-config) installed to share/cmake
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
@@ -8,18 +7,14 @@ get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative part
 string(TOUPPER ${prj} PRJ)
 set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
 set(verDir /${prj}_@VER@)
-unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR eigen3/Eigen/Eigen PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
-list(APPEND ${PRJ}_INCLUDE_DIR ${XP_ROOTDIR}/include${verDir}/eigen3)
+set(includeDirs ${XP_ROOTDIR}/include${verDir} ${XP_ROOTDIR}/include${verDir}/eigen3)
 if(NOT TARGET xpro::eigen)
   add_library(xpro::eigen INTERFACE IMPORTED)
-  if(${PRJ}_INCLUDE_DIR)
-    set_target_properties(xpro::eigen PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${${PRJ}_INCLUDE_DIR}"
-      )
-  endif()
+  set_target_properties(xpro::eigen PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${includeDirs}"
+    )
 endif()
-set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR)
+set(reqVars ${PRJ}_VER)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
 mark_as_advanced(${reqVars})
