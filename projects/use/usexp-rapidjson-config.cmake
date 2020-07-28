9dcb399a09cab33c43d55b3ba4816e022f970e69
Scott M Anderson <smanders@sdl.usu.edu>
2020-07-28 12:46:06 -0600

    simplify use script: remove RAPIDJSON_INCLUDE_DIR
    issue https://github.com/smanders/externpro/issues/273

--- projects/use/usexp-rapidjson-config.cmake
+++ projects/use/usexp-rapidjson-config.cmake
@@ -1,24 +1,18 @@
 # RAPIDJSON_FOUND - RapidJSON was found
 # RAPIDJSON_VER - RapidJSON version
-# RAPIDJSON_INCLUDE_DIR - the RapidJSON include directory
 set(prj rapidjson)
 # this file (-config) installed to share/cmake
 get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
 string(TOUPPER ${prj} PRJ)
 set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
-set(verDir /${prj}_@VER@)
-unset(${PRJ}_INCLUDE_DIR CACHE)
-find_path(${PRJ}_INCLUDE_DIR rapidjson/rapidjson.h PATHS ${XP_ROOTDIR}/include${verDir} NO_DEFAULT_PATH)
 if(NOT TARGET xpro::rapidjson)
   add_library(xpro::rapidjson INTERFACE IMPORTED)
-  if(${PRJ}_INCLUDE_DIR)
-    set_target_properties(xpro::rapidjson PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${${PRJ}_INCLUDE_DIR}"
-      )
-  endif()
+  set_target_properties(xpro::rapidjson PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${XP_ROOTDIR}/include/${prj}_@VER@"
+    )
 endif()
-set(reqVars ${PRJ}_VER ${PRJ}_INCLUDE_DIR)
+set(reqVars ${PRJ}_VER)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
 mark_as_advanced(${reqVars})
