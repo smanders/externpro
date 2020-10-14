7cacf68084aa2543a8020b2d23a174c8926b3360
cfrandsen <Cameron.Frandsen@sdl.usu.edu>
2020-10-14 10:16:55 -0600

    Update protobuf from 3.0.0-beta-1 to 3.13.0.0
    Currently maintaining namespace xpro(even though protobuf now has a namespace protobuf)
    Current we changing the install step to not put down symlinks.
    I have removed SYSTEM_LIBRARIES as it builds on centos and ubuntu
    removing .pc files

--- projects/protobuf.cmake
+++ projects/protobuf.cmake
@@ -1,8 +1,8 @@
 # protobuf
 xpProOption(protobuf DBG)
-set(VER 3.0.0-beta-1)
+set(VER 3.13.0)
 set(REPO github.com/protocolbuffers/protobuf)
-set(FORK github.com/smanders/protobuf)
+set(FORK github.com/cameronfrandsen/protobuf)
 set(PRO_PROTOBUF
   NAME protobuf
   WEB "protobuf" https://developers.google.com/protocol-buffers/ "Protocol Buffers website"
@@ -16,7 +16,7 @@ set(PRO_PROTOBUF
   GIT_TAG xp${VER} # what to 'git checkout'
   GIT_REF v${VER} # create patch from this tag to 'git checkout'
   DLURL https://${REPO}/archive/v${VER}.tar.gz
-  DLMD5 63aad3f1814b5c6cd06c7712cd5ba9db
+  DLMD5 1a6274bc4a65b55a6fa70e264d796490
   DLNAME protobuf-${VER}.tar.gz
   PATCH ${PATCH_DIR}/protobuf.patch
   DIFF https://${FORK}/compare/protocolbuffers:
@@ -32,11 +32,12 @@ function(build_protobuf)
     @ONLY NEWLINE_STYLE LF
     )
   set(XP_CONFIGURE
-    -DBUILD_TESTING=OFF # we don't have gmock, unless we switch to a release tar ball
+    -Dprotobuf_BUILD_TESTS=OFF # we don't have gmock, unless we switch to a release tar ball
     -DZLIB_MODULE_PATH=ON # with this option ON, we don't need -DZLIB=ON
     -DCMAKE_INSTALL_LIBDIR=lib # without this *some* platforms (RHEL, but not Ubuntu) install to lib64
     -DXP_NAMESPACE:STRING=xpro
-    -DPROTOBUF_VER=${VER}
+    -DPROTOBUF_VER=${VER}.0
+    -DCMAKE_INSTALL_CMAKEDIR=lib/cmake/protobuf_${VER}
     )
   xpCmakeBuild(protobuf "${depTgts}" "${XP_CONFIGURE}")
 endfunction()
