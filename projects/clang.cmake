a55f9c964ac4e0b516a183bb9945c5ad4459a5bd
Scott M Anderson <smanders@sdl.usu.edu>
2021-04-28 00:29:22 +0000

    update llvm/clang repo
    issue https://github.com/smanders/externpro/issues/312

--- projects/clang.cmake
+++ projects/clang.cmake
@@ -1,8 +1,6 @@
 # clang
 include(${CMAKE_CURRENT_LIST_DIR}/llvm.cmake)
 xpGetArgValue(${PRO_LLVM} ARG VER VALUE llvmVer)
-set(REPO github.com/llvm-mirror/clang)
-set(FORK github.com/smanders/clang)
 set(PRO_CLANG
   NAME clang
   SUPERPRO llvm
@@ -10,13 +8,8 @@ set(PRO_CLANG
   WEB "clang" http://clang.llvm.org/ "clang website"
   LICENSE "open" "http://clang.llvm.org/features.html#license" "LLVM 'BSD' License"
   DESC "clang: a C language family frontend for LLVM"
-  REPO "repo" https://${REPO} "clang repo on github"
   GRAPH GRAPH_SHAPE box BUILD_DEPS llvm
   VER ${llvmVer}
-  GIT_ORIGIN git://${FORK}.git
-  GIT_UPSTREAM git://${REPO}.git
-  # NOTE: forked repo (we used to patch clang), upstream doesn't have tag
-  GIT_TAG v${llvmVer} # what to 'git checkout'
   DLURL http://llvm.org/releases/${llvmVer}/cfe-${llvmVer}.src.tar.xz
   DLMD5 29e1d86bee422ab5345f5e9fb808d2dc
   )
--- projects/llvm.cmake
+++ projects/llvm.cmake
@@ -1,18 +1,17 @@
 # llvm
 xpProOption(llvm)
 set(VER 3.9.0)
-string(REGEX REPLACE "([0-9]+)\\.([0-9]+)(\\.[0-9]+)?" "release_\\1\\2" LLVM_BRANCH ${VER})
-set(REPO github.com/llvm-mirror/llvm)
+set(REPO github.com/llvm/llvm-project)
 set(PRO_LLVM
   NAME llvm
   WEB "LLVM" http://llvm.org/ "LLVM website"
-  LICENSE "open" http://llvm.org/svn/llvm-project/llvm/trunk/LICENSE.TXT "LLVM Release License"
+  LICENSE "open" https://releases.llvm.org/${VER}/LICENSE.TXT "LLVM Release License"
   DESC "The LLVM Compiler Infrastructure"
   REPO "repo" https://${REPO} "llvm repo on github"
   GRAPH GRAPH_SHAPE box
   VER ${VER}
   GIT_ORIGIN git://${REPO}.git
-  GIT_TAG ${LLVM_BRANCH}
+  GIT_TAG llvmorg-${VER}
   DLURL http://llvm.org/releases/${VER}/llvm-${VER}.src.tar.xz
   DLMD5 f2093e98060532449eb7d2fcfd0bc6c6
   SUBPRO clang
