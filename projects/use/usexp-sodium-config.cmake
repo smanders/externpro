85b43d38a8ba65ae4dc784deb2135310c9b7a22a
Scott M Anderson <smanders@sdl.usu.edu>
2021-07-14 16:05:39 -0600

    sodium use script: sodium depends on Threads
    things that depend on sodium won't need to find Threads::Threads now
    issue https://github.com/smanders/externpro/issues/317

--- projects/use/usexp-sodium-config.cmake
+++ projects/use/usexp-sodium-config.cmake
@@ -7,6 +7,8 @@ get_filename_component(XP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(XP_ROOTDIR ${XP_ROOTDIR} ABSOLUTE) # remove relative parts
 string(TOUPPER ${prj} PRJ)
 set(${PRJ}_VER "@VER@ [@PROJECT_NAME@]")
+set(THREAD_PREFER_PTHREAD_FLAG ON)
+find_package(Threads REQUIRED) # sodium depends on Threads::Threads
 # targets file (-targets) installed to lib/cmake
 include(${XP_ROOTDIR}/lib/cmake/${prj}_@VER@-targets.cmake)
 set(${PRJ}_LIBRARIES xpro::${prj})
