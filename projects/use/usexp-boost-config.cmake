0ba0f2535b700462b2f6cea68caa57e1edd6b1fd
Scott M Anderson <smanders@sdl.usu.edu>
2021-08-20 21:14:05 +0000

    simlify boost use script, use BoostConfig.cmake from boost build
    https://github.com/smanders/externpro/issues/308#issuecomment-902959955

--- projects/use/usexp-boost-config.cmake
+++ projects/use/usexp-boost-config.cmake
@@ -14,13 +14,15 @@ else()
 endif()
 set(BOOST_VERSION "${BOOST_VER} [@PROJECT_NAME@]")
 if(NOT DEFINED Boost_LIBS)
-  # dependencies determined by examining cmake's Modules/FindBoost.cmake
+  # dependencies determined by 'grep _DEPS boost_*/*-static.cmake'
   set(Boost_LIBS   # dependency order
+    log_setup      # dep:log
+    ######
     log            # dep:thread
     ######
-    iostreams      # dep:regex
+    graph          # dep:regex
     json           # dep:container
-    thread         # dep:atomic,chrono,date_time
+    thread         # dep:atomic
     timer          # dep:chrono
     wserialization # dep:serialization
     ######
@@ -28,10 +30,8 @@ if(NOT DEFINED Boost_LIBS)
     chrono
     container
     date_time
-    exception
     filesystem
-    graph
-    log_setup
+    iostreams
     nowide
     program_options
     random
@@ -47,83 +47,19 @@ if(NOT ${idx} EQUAL -1)
   # iostreams depends on zlib bzip2
   xpFindPkg(PKGS bzip2 zlib) # dependencies
 endif()
-unset(Boost_INCLUDE_DIR CACHE)
-unset(Boost_LIBRARY_DIR CACHE)
-foreach(lib ${Boost_LIBS})
-  string(TOUPPER ${lib} UPPERLIB)
-  unset(Boost_${UPPERLIB}_LIBRARY_DEBUG CACHE)
-  unset(Boost_${UPPERLIB}_LIBRARY_RELEASE CACHE)
-endforeach()
-string(REGEX REPLACE "([0-9]+)\\.([0-9]+)(\\.[0-9]+)?" "\\1.\\2" BOOST_VER2 ${BOOST_VER})
-# see FindBoost.cmake for details on the following variables
-set(Boost_ADDITIONAL_VERSIONS ${BOOST_VER} ${BOOST_VER2})
+# see BoostConfig.cmake for details on the following variables
 set(Boost_FIND_QUIETLY TRUE)
-set(Boost_NO_SYSTEM_PATHS TRUE)
 set(Boost_USE_STATIC_LIBS ON)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME ON)
-#set(Boost_DEBUG TRUE) # enable debugging output of FindBoost.cmake
-#set(Boost_DETAILED_FAILURE_MSG) # output detailed information
-set(BOOST_ROOT ${XP_ROOTDIR})
-# TODO: remove the following once CMAKE_CXX_COMPILER_ARCHITECTURE_ID is defined for all supported compilers
-#   https://gitlab.kitware.com/cmake/cmake/issues/17702
-# boost versions >= 1.66.0 add an additional modifier to the library name and some compilers don't set the
-# cmake variable used in FindBoost.cmake that's used to set _boost_ARCHITECTURE_TAG
-#   https://gitlab.kitware.com/cmake/cmake/issues/17701
-if(BOOST_VER VERSION_GREATER_EQUAL "1.66.0" AND "x${CMAKE_CXX_COMPILER_ARCHITECTURE_ID}" STREQUAL "x")
-  execute_process(COMMAND uname --machine
-    OUTPUT_VARIABLE unameMachine
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_VARIABLE unameErr
-    )
-  if(DEFINED unameMachine AND NOT unameErr)
-    if(unameMachine STREQUAL x86_64)
-      set(CMAKE_CXX_COMPILER_ARCHITECTURE_ID x64)
-    elseif(unameMachine MATCHES "^armv" OR unameMachine MATCHES "^aarch")
-      set(CMAKE_CXX_COMPILER_ARCHITECTURE_ID "ARM")
-    endif()
-  endif()
-endif()
-find_package(Boost ${BOOST_VER} REQUIRED COMPONENTS ${Boost_LIBS})
-set(${PRJ}_FOUND ${Boost_FOUND})
-set(${PRJ}_INCLUDE_DIR ${Boost_INCLUDE_DIRS})
-function(listPrependToAll var prefix)
-  set(listVar)
-  foreach(f ${ARGN})
-    list(APPEND listVar "${prefix}${f}")
-  endforeach()
-  set(${var} "${listVar}" PARENT_SCOPE)
-endfunction()
-listPrependToAll(boostLibs "Boost::" ${Boost_LIBS})
-# TRICKY: Boost_LIBRARIES returned from find_package() introduces hard-coded absolute
-# paths and wrecks havoc on cmake-generated targets files, hence boostLibs
-set(${PRJ}_LIBRARIES ${boostLibs})
-set(reqVars ${PRJ}_VERSION ${PRJ}_INCLUDE_DIR ${PRJ}_LIBRARIES)
-if(UNIX)
-  include(CheckLibraryExists)
-  function(checkLibraryConcat lib symbol liblist)
-    string(TOUPPER ${lib} LIB)
-    check_library_exists("${lib}" "${symbol}" "" XP_BOOST_HAS_${LIB})
-    if(XP_BOOST_HAS_${LIB})
-      list(APPEND ${liblist} ${lib})
-      set(${liblist} ${${liblist}} PARENT_SCOPE)
-    endif()
-  endfunction()
-  checkLibraryConcat(m pow syslibs)
-  checkLibraryConcat(pthread pthread_create syslibs)
-  checkLibraryConcat(rt shm_open syslibs)
-  list(APPEND ${PRJ}_LIBRARIES ${syslibs})
-  checkLibraryConcat(rt clock_gettime additionalThreadLibs)
-  if(DEFINED additionalThreadLibs AND TARGET Boost::thread)
-    get_target_property(libs Boost::thread INTERFACE_LINK_LIBRARIES)
-    if(libs)
-      list(APPEND libs ${additionalThreadLibs})
-    else()
-      set(libs ${additionalThreadLibs})
-    endif()
-    set_target_properties(Boost::thread PROPERTIES INTERFACE_LINK_LIBRARIES "${libs}")
-  endif()
-endif()
+#set(Boost_VERBOSE TRUE) # enable verbose output of BoostConfig.cmake
+#set(Boost_DEBUG TRUE) # enable debug (even more verbose) output of BoostConfig.cmake
+find_package(Boost ${BOOST_VER} REQUIRED COMPONENTS ${Boost_LIBS}
+  PATHS ${XP_ROOTDIR}/lib/cmake/Boost-${BOOST_VER} NO_DEFAULT_PATH
+  )
+mark_as_advanced(Boost_DIR)
+set(${PRJ}_LIBRARIES ${Boost_LIBRARIES})
+set(reqVars ${PRJ}_VERSION ${PRJ}_LIBRARIES)
 include(FindPackageHandleStandardArgs)
 set(FPHSA_NAME_MISMATCHED TRUE) # find_package_handle_standard_args NAME_MISMATCHED (prefix usexp-)
 find_package_handle_standard_args(${prj} REQUIRED_VARS ${reqVars})
