2074bfbbc30b3e6fcd221c0079e0f006ffb676f8
Scott M Anderson <smanders@sdl.usu.edu>
2022-08-26 17:36:04 -0600

    project simplification: rapidjson
    issue https://github.com/smanders/externpro/issues/356

--- projects/rapidjson.cmake
+++ projects/rapidjson.cmake
@@ -20,19 +20,22 @@ function(build_rapidjson)
   if(NOT (XP_DEFAULT OR XP_PRO_RAPIDJSON))
     return()
   endif()
+  xpGetArgValue(${PRO_RAPIDJSON} ARG NAME VALUE NAME)
   xpGetArgValue(${PRO_RAPIDJSON} ARG VER VALUE VER)
-  set(verDir /rapidjson_${VER})
-  configure_file(${PRO_DIR}/use/usexp-rapidjson-config.cmake ${STAGE_DIR}/share/cmake/
+  set(LIBRARY_HDR xpro::${NAME})
+  set(LIBRARY_INCLUDEDIRS include/${NAME}_${VER})
+  configure_file(${PRO_DIR}/use/usexp-template-hdr-config.cmake
+    ${STAGE_DIR}/share/cmake/usexp-${NAME}-config.cmake
     @ONLY NEWLINE_STYLE LF
     )
-  ExternalProject_Get_Property(rapidjson SOURCE_DIR)
-  ExternalProject_Add(rapidjson_bld DEPENDS rapidjson
-    DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${DWNLD_DIR} CONFIGURE_COMMAND ""
+  ExternalProject_Get_Property(${NAME} SOURCE_DIR)
+  ExternalProject_Add(${NAME}_bld DEPENDS ${NAME}
+    DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR} CONFIGURE_COMMAND ""
     SOURCE_DIR ${SOURCE_DIR} BINARY_DIR ${NULL_DIR} INSTALL_DIR ${NULL_DIR}
     BUILD_COMMAND ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/include ${STAGE_DIR}/include${verDir}
+      <SOURCE_DIR>/include ${STAGE_DIR}/${LIBRARY_INCLUDEDIRS}
     INSTALL_COMMAND ""
     )
-  set_property(TARGET rapidjson_bld PROPERTY FOLDER ${bld_folder})
-  message(STATUS "target rapidjson_bld")
+  set_property(TARGET ${NAME}_bld PROPERTY FOLDER ${bld_folder})
+  message(STATUS "target ${NAME}_bld")
 endfunction()
