77c5545c330de0f7673da162877d77e0b6564f3c
Scott M Anderson <smanders@sdl.usu.edu>
2022-08-26 17:45:04 -0600

    project simplification: rapidxml
    issue https://github.com/smanders/externpro/issues/356

--- projects/rapidxml.cmake
+++ projects/rapidxml.cmake
@@ -23,19 +23,22 @@ function(build_rapidxml)
   if(NOT (XP_DEFAULT OR XP_PRO_RAPIDXML))
     return()
   endif()
+  xpGetArgValue(${PRO_RAPIDXML} ARG NAME VALUE NAME)
   xpGetArgValue(${PRO_RAPIDXML} ARG VER VALUE VER)
-  set(verDir /rapidxml_${VER})
-  configure_file(${PRO_DIR}/use/usexp-rapidxml-config.cmake ${STAGE_DIR}/share/cmake/
+  set(LIBRARY_HDR xpro::${NAME})
+  set(LIBRARY_INCLUDEDIRS include/${NAME}_${VER})
+  configure_file(${PRO_DIR}/use/usexp-template-hdr-config.cmake
+    ${STAGE_DIR}/share/cmake/usexp-${NAME}-config.cmake
     @ONLY NEWLINE_STYLE LF
     )
-  ExternalProject_Get_Property(rapidxml SOURCE_DIR)
-  ExternalProject_Add(rapidxml_bld DEPENDS rapidxml
+  ExternalProject_Get_Property(${NAME} SOURCE_DIR)
+  ExternalProject_Add(${NAME}_bld DEPENDS ${NAME}
     DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR} CONFIGURE_COMMAND ""
     SOURCE_DIR ${SOURCE_DIR} BINARY_DIR ${NULL_DIR} INSTALL_DIR ${NULL_DIR}
     BUILD_COMMAND ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR> ${STAGE_DIR}/include${verDir}/rapidxml
+      <SOURCE_DIR> ${STAGE_DIR}/${LIBRARY_INCLUDEDIRS}/${NAME}
     INSTALL_COMMAND ""
     )
-  set_property(TARGET rapidxml_bld PROPERTY FOLDER ${bld_folder})
-  message(STATUS "target rapidxml_bld")
+  set_property(TARGET ${NAME}_bld PROPERTY FOLDER ${bld_folder})
+  message(STATUS "target ${NAME}_bld")
 endfunction()
