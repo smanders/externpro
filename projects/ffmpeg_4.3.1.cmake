15597c1681de74e14ce08ee8b97e57c0e584d45c
Scott M Anderson <smanders@sdl.usu.edu>
2023-06-22 17:46:14 -0600

    ffmpeg: smanders FORK 4.3.1

--- projects/ffmpeg_4.3.1.cmake
+++ projects/ffmpeg_4.3.1.cmake
@@ -1,23 +1,24 @@
 # ffmpeg
 set(VER ${FFMPEG_NEWVER})
 xpProOption(ffmpeg_${VER})
-set(REPO github.com/FFmpeg/FFmpeg)
-set(FORK github.com/ndrasmussen/FFmpeg)
+set(REPO https://github.com/FFmpeg/FFmpeg)
+set(FORK https://github.com/smanders/FFmpeg)
+# https://github.com/ndrasmussen/FFmpeg/tree/xp4.3.1
 set(PRO_FFMPEG_${VER}
   NAME ffmpeg_${VER}
   WEB "ffmpeg" https://www.ffmpeg.org/ "ffmpeg website"
   LICENSE "LGPL" https://www.ffmpeg.org/legal.html "LGPL v2.1"
   DESC "complete, cross-platform solution to record, convert and stream audio and video"
-  REPO "repo" https://${REPO} "ffmpeg repo on github"
+  REPO "repo" ${REPO} "ffmpeg repo on github"
   #GRAPH GRAPH_NODE ffmpeg
   BUILD_DEPS openh264 yasm
   VER ${VER}
-  GIT_ORIGIN git://${FORK}.git
-  GIT_UPSTREAM git://${REPO}.git
+  GIT_ORIGIN ${FORK}
+  GIT_UPSTREAM ${REPO}
   GIT_TAG xp${VER} # what to 'git checkout'
   GIT_REF n${VER} # create patch from this tag to 'git checkout'
   DLURL http://ffmpeg.org/releases/ffmpeg-${VER}.tar.bz2
   DLMD5 804707549590e90880e8ecd4e5244fd8
   PATCH ${PATCH_DIR}/ffmpeg_${VER}.patch
-  DIFF https://${FORK}/compare/FFmpeg:
+  DIFF ${FORK}/compare/FFmpeg:
   )
