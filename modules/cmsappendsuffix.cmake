2e1a74d4f48662cc63a859812f05b65686efcfd5
Scott M Anderson <smanders@sdl.usu.edu>
2024-01-22 17:50:42 -0700

    git rm -r modules
    * now located in .devcontainer/cmake/

--- modules/cmsappendsuffix.cmake
+++
@@ -1,7 +0,0 @@
-file(GLOB files ${src})
-foreach(f ${files})
-  get_filename_component(dir ${f} DIRECTORY)
-  get_filename_component(ext ${f} EXT)
-  get_filename_component(nam ${f} NAME_WE)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${f} ${dir}/${nam}${suffix}${ext})
-endforeach()
--- modules/cmsconfigurefile.cmake
+++
@@ -1,4 +0,0 @@
-# @param[in] input
-# @param[in] output
-# @param[in] any other defines that configure_file needs
-configure_file(${input} ${output} @ONLY NEWLINE_STYLE LF)
